4e1f836d284c5258acb87c913ef6b59faeda650b
Giovanni Alcantara <me@giovannialcantara.com>
2016-10-29 23:57:01 -0500

    Create settings.json, fix and refactor init.js

--- server/init.js
+++ server/init.js
@@ -1,22 +1,44 @@
 import linkedInProfile from 'linkedin-public-profile';
 
-Meteor.startup = function() {
+Meteor.startup(function() {
+    console.log("sup");
+
+    ServiceConfiguration.configurations.update(
+        { "service": "linkedin" },
+        {
+            $set: {
+                "clientId": Meteor.settings.linkedInClientId,
+                "secret": Meteor.settings.linkedInSecret
+            }
+        },
+        { upsert: true }
+    );
+
     Accounts.config({
         sendVerificationEmail: true,
         forbidClientAccountCreation: false,
-        requestPermissions: {
-            linkedin: ['r_basicprofile']
-        },
     });
 
-    AdminConfig = {
-        adminEmails: ['me@giovannialcantara.com'],
+    Accounts.emailTemplates.siteName = "Mentuit";
+    Accounts.emailTemplates.from     = "Mentuit <noreply@mentuit.com>";
+    Accounts.emailTemplates.verifyEmail = {
+        subject() {
+            return "Welcome to Mentuit!";
+        },
+        text( user, url ) {
+            let emailAddress   = user.emails[0].address,
+                urlWithoutHash = url.replace( '#/', '' ),
+                supportEmail   = "support@mentuit.com",
+                emailBody      = `To verify your email address (${emailAddress}) click the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+
+            return emailBody;
+        }
     };
 
     const url = 'https://www.linkedin.com/in/giovannialcantara';
 
-// standard usage
     linkedInProfile(url).then(profile => {  // chain your logic
         console.log(profile);
     });
-};
+
+});
